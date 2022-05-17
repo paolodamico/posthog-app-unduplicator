f1823c4a2d08770f01ee94105126b7570fd83868
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-05-17 08:30:08 -0500

    feat: readme, logo & clean up logging

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-TBD
+<img src="logo.png" alt="Unduplicates plugin logo" height="100" />
+
+# PostHog Community Plugin: Unduplicates
+
+This plugin helps prevent duplicate events from being ingested into PostHog. It's particularly helpful if you're backfilling information as you're already ingesting ongoing events. The plugin scopes duplicates for each individual project. The plugin has two modes.
+
+-   **Event and Timestamp**. An event will be treated as duplicate if the timestamp, event name, user's distinct ID matches exactly (in the scope of each project), regardless of what properties are included.
+-   **All Properties**. An event will be treated as duplicate only all properties match exactly, as well as the timestamp, event name, user's distinct ID.
+
+The plugin uses temporary cache to perform faster checks and uses PostHog's API to check if duplicate events have already been ingested.
+
+## 🚀 Usage
+
+To use it simply install the plugin from the repository URL: https://github.com/paolodamico/posthog-plugin-unduplicates or search for it in the PostHog Plugin Library.
+
+## 🧑‍💻 Development & testing
+
+Contributions are welcomed! Feel free to open a PR or an issue. To develop locally and contribute to this package, you can simply follow these instructions after clonning the repo.
+
+-   Install dependencies
+    ```bash
+    yarn install
+    ```
+-   Run tests
+    ```bash
+    yarn test
+    ```
+-   Install plugin in your local instance by going to `/project/plugins` in your PostHog instance, clicking on the "Advanced" tab and entering the full path where you cloned the repo. Please note that running plugins locally on PostHog is currently buggy (see [posthog#7170](https://github.com/PostHog/posthog/issues/7170)).
+
+## 🧑‍⚖️ License
+
+This repository is MIT licensed. Please review the LICENSE file in this repository.
+
+Copyright (C) 2022 Paolo D'Amico.
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Plugin, PluginEvent } from '@posthog/plugin-scaffold'
-import { createHash } from 'crypto'
+import { createHash, randomUUID } from 'crypto'
 import { URLSearchParams } from 'url'
 
 interface UnduplicatesPluginInterface {
@@ -8,15 +8,20 @@ interface UnduplicatesPluginInterface {
     }
 }
 
-const logDuplicate = (event: PluginEvent, location: 'cache' | 'API'): void => {
-    console.log(
-        `Prevented duplicate event ingestion from ${location}. ${event.event} @ ${event.timestamp} for user ${event.distinct_id} on Project ID ${event.team_id}`
-    )
+const stringifyEvent = (event: PluginEvent): string => {
+    return `(${randomUUID().toString()}; project #${event.team_id}). Event "${event.event}" @ ${
+        event.timestamp
+    } for user ${event.distinct_id}.`
+}
+
+const logDuplicate = (stringifiedEvent: string, location: 'cache' | 'API'): void => {
+    console.info(`Prevented duplicate event ingestion from ${location}. ${stringifiedEvent}`)
 }
 
 const plugin: Plugin<UnduplicatesPluginInterface> = {
     processEvent: async (event, { cache, config }) => {
-        console.debug(`Beginning processing for ${event.event}`)
+        const stringifiedEvent = stringifyEvent(event)
+        console.debug(`Beginning processing. ${stringifiedEvent}`)
 
         if (!event.timestamp) {
             console.warn('Received event without a timestamp, this plugin will not work without it. Skipping.')
@@ -32,7 +37,7 @@ const plugin: Plugin<UnduplicatesPluginInterface> = {
 
         const cachedEvent = await cache.get(eventKey, null)
         if (cachedEvent) {
-            logDuplicate(event, 'cache')
+            logDuplicate(stringifiedEvent, 'cache')
             return null
         }
 
@@ -56,11 +61,11 @@ const plugin: Plugin<UnduplicatesPluginInterface> = {
                 if (potentialMatch.timestamp === event.timestamp) {
                     if (config.dedupMode === 'All Properties') {
                         if (JSON.stringify(event.properties) === JSON.stringify(potentialMatch.properties)) {
-                            logDuplicate(event, 'API')
+                            logDuplicate(stringifiedEvent, 'API')
                             return null
                         }
                     } else {
-                        logDuplicate(event, 'API')
+                        logDuplicate(stringifiedEvent, 'API')
                         return null
                     }
                 }
